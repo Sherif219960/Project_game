a0100291e85ca6ae314c0041cb835a9b07d42409
sherif anwarm <sherifanwar76@gmail.clomm>
2023-12-24 04:11:26 +0200

    make update for Nave bar

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,10 +1,14 @@
 import './index.css'
+import logo from '../../assets/images/logo.png'
+import proheader from '../../assets/images/profile-header.jpg'
 
 const Header = () => {
   return (
     <nav className="navbar navbar-expand-lg bg-dark border-bottom border-body" data-bs-theme="dark">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="#">Navbar</a>
+      <div className="container">
+        <a className="navbar-brand" href="#">
+          <img src={logo} alt="" />
+        </a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
@@ -14,13 +18,17 @@ const Header = () => {
               <a className="nav-link active" aria-current="page" href="#">Home</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Features</a>
+              <a className="nav-link" href="#">Browse</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Pricing</a>
+              <a className="nav-link" href="#">Details</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">About</a>
+              <a className="nav-link" href="#">Streams</a>
+            </li>
+            <li className="nav-item profile">
+              <a className="nav-link" href="#">Porfile</a>
+              <img src={proheader} alt="fight" />
             </li>
           </ul>
         </div>
